f30cd4241bea6ab2194e749a847460a3c3f1776e
merima98 <merimaceranic98@gmail.com>
2021-02-19 16:24:26 +0100

    Adding global styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import "./App.css";
 
 import Home from "./features/home/Home";
 import Quotes from "./features/quotes/Quotes";
-const queryClient = new QueryClient();
+import GlobalStyle from "./globalStyles.js";
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/quotes" component={Quotes} />
-        </Switch>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/quotes" component={Quotes} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
